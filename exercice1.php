34aef4eb7741bfe832db62add8adebd11921affc
orogarde <fredericlepichonpro@gmail.com>
2017-11-16 12:21:05 +0100

    version1.1

--- exercice1.php
+++ exercice1.php
@@ -1,4 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1> TP GIT 2 AVANCE </h1>
+<h2> Une société qui loue des bateaux de plaisance veut connaitre les bateaux
+ qui peuvent embarquer plus de 10 passagers </h2>
+ <?php
 $tableau= array( 
     "Manuevra"=> array("nbEquipage"=>3,"passager"=>4) ,
     "Sirius"=>array("nbEquipage"=>4,"passager"=>6) ,
@@ -16,4 +28,6 @@ $tableau= array(
     
 
 
-?>
+?>   
+</body>
+</html>
