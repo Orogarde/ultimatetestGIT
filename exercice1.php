33b7a84916097a71f7b92f3e50939ed845a9a326
orogarde <fredericlepichonpro@gmail.com>
2017-11-16 14:23:11 +0100

    version 1.3 master

--- exercice1.php
+++ exercice1.php
@@ -11,21 +11,29 @@
 <h2> Une société qui loue des bateaux de plaisance veut connaitre les bateaux
  qui peuvent embarquer plus de 10 passagers </h2>
  <?php
+$total=0;
 $tableau= array( // création du tableau associatif #1
     "Manuevra"=> array("nbEquipage"=>3,"passager"=>4) ,  //sous-tableau associatif
     "Sirius"=>array("nbEquipage"=>4,"passager"=>6) ,    //sous-tableau associatif
-    "Surcouf"=>array("nbEquipage"=>8,"passager"=>15)    //sous-tableau associatif
-    "Nautilus"=>array("nbEquipage"=>5,"passager"=>5), 
+    "Surcouf"=>array("nbEquipage"=>8,"passager"=>15),    //sous-tableau associatif
+    "Nautilus"=> array("nbEquipage"=>5,"passager"=>5), 
     );
     foreach ($tableau as $nomBateau => $infoEquipage) { //parcours du tableau principal
-            echo $nomBateau." a ".$infoEquipage["passager"]." passagers ".'<br>'; 
+            echo $nomBateau." a ".$infoEquipage["passager"]." passagers ".'<br>';
           if($infoEquipage["passager"]>10) // si le nombre de passager est supérieur à 10 ...
           {
-              echo "les Bateaux qui peuvent embarquer plus de 10 passagers sont : ".$nomBateau.'<br>'; // ... affichage du bateau en question
+            
+            echo "les Bateaux qui peuvent embarquer plus de 10 passagers sont : ".$nomBateau.'<br>'; // ... affichage du bateau en question
           }
+        }
+
         //  echo  $nomBateau.'<br>';
         //  echo $infoEquipage["nbEquipage"]." ".$infoEquipage['passager'].'<br>';
+    foreach ($tableau as $nomBateau => $infoEquipage) 
+    {
+       $total += $infoEquipage["nbEquipage"];
     }
+    echo "nombre totals d'equipages: ".$total;
     
 
 
